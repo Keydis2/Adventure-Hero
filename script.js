ce7c8843f6113e0f155b0d4f12091327ee5523df
Edward Ittermann <edward.ittermann@dots-it.de>
2022-11-28 15:07:00 +0100

    first code

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 1600;
+  canvas.height = 800;
+
+  class Inputhandler {
+    constructor() {
+      this.keys = [];
+    }
+  }
+
+  class Player {}
+
+  class Background {}
+
+  class Enemy {}
+
+  function handleEnemies() {}
+
+  function displayStatusText() {}
+
+  function animate() {}
+});
